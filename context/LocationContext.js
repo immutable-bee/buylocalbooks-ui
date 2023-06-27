8a768f8e2a53451494f604d018b7ae35e4222309
immutable-bee <immutablebee@gmail.com>
2023-06-27 16:29:59 -0600

    update provider/add provider routes

--- context/LocationContext.js
+++ context/LocationContext.js
@@ -6,23 +6,59 @@ export const LocationProvider = ({ children }) => {
   const [location, setLocation] = useState();
 
   // Function to get the user's location
-  const getLocation = () => {
+  const getLocation = async () => {
     // Try to get the location from Session Storage first
     const storedLocation = sessionStorage.getItem("location");
 
-    if (storedLocation) {
+    if (storedLocation?.postalCode) {
       setLocation(JSON.parse(storedLocation));
     } else if (navigator.geolocation) {
       // If no stored location, ask the user for their location
-      navigator.geolocation.getCurrentPosition((position) => {
+      navigator.geolocation.getCurrentPosition(async (position) => {
         const newLocation = {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
         };
+
+        // Call the route to get the postal code
+        const response = await fetch("/api/geocoding/get-zip", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newLocation),
+        });
+        const data = await response.json();
+
+        // If the request was successful, add the postal code to the location
+        if (response.ok) {
+          newLocation.postalCode = data.postalCode;
+        }
+
+        // Set and store the new location
         setLocation(newLocation);
-        // Store the new location in Session Storage
         sessionStorage.setItem("location", JSON.stringify(newLocation));
       });
+    }
+  };
+
+  const setLocationByZip = async (zip) => {
+    const response = await fetch("/api/geocoding/get-coords", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ postalCode: zip }),
+    });
+
+    if (response.ok) {
+      const newLocation = await response.json();
+      newLocation.postalCode = zip;
+      setLocation(newLocation);
+      // Store the new location in Session Storage
+      sessionStorage.setItem("location", JSON.stringify(newLocation));
+    } else {
+      // Handle error...
     }
   };
 
@@ -32,7 +68,9 @@ export const LocationProvider = ({ children }) => {
   }, []);
 
   return (
-    <LocationContext.Provider value={{ location, getLocation }}>
+    <LocationContext.Provider
+      value={{ location, getLocation, setLocationByZip }}
+    >
       {children}
     </LocationContext.Provider>
   );
---
+++ pages/api/geocoding/get-zip.js
@@ -0,0 +1,32 @@
+const handler = async (req, res) => {
+  const { latitude, longitude } = req.body;
+
+  const apiKey = process.env.GMAPS_KEY;
+  const endpoint = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`;
+
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  const results = data.results;
+  const firstResult = results[0];
+  if (!firstResult) {
+    res.status(404).json({ message: "No location found" });
+    return;
+  }
+
+  // Get the postal code from the address components
+  const postalCodeComponent = firstResult.address_components.find((component) =>
+    component.types.includes("postal_code")
+  );
+  if (!postalCodeComponent) {
+    res.status(404).json({ message: "No postal code found" });
+    return;
+  }
+
+  const postalCode = postalCodeComponent.long_name;
+
+  // Return the postal code
+  res.status(200).json({ postalCode });
+};
+
+export default handler;
