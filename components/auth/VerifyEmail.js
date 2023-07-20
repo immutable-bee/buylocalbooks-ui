d5275447a2433acaed26977c3516b3466ae70998
immutable-bee <immutablebee@gmail.com>
2023-07-20 17:20:34 -0600

    extract verify email case into component

---
+++ components/auth/VerifyEmail.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { applyActionCode } from "firebase/auth";
+import { useRouter } from "next/router";
+import Loading from "../utility/Loading";
+
+const VerifyEmail = ({ auth, actionCode }) => {
+  const router = useRouter();
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (actionCode) {
+      applyActionCode(auth, actionCode)
+        .then(() => {
+          setVerified(true);
+          setTimeout(() => {
+            router.push("/");
+          }, [5000]);
+        })
+        .catch((error) => {
+          setError(error);
+        });
+    }
+  }, [actionCode]);
+
+  return (
+    <div className="min-h-screen bg-pink-100 flex items-center justify-center">
+      <div className="bg-white shadow-lg p-8 rounded-lg">
+        {verified ? (
+          <>
+            <h1 className="text-3xl text-purple-600 mb-8">
+              Congratulations! Your email has been successfully verified.
+            </h1>
+            <div className="flex justify-center gap-2">
+              <h1 className="text-2xl">Redirecting</h1>
+              <div className="pt-4">
+                <Loading />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="flex justify-center gap-2">
+            <h1 className="text-2xl">Authenticating</h1>
+            <div className="pt-3">
+              <Loading />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default VerifyEmail;
--- pages/auth/firebase/index.js
+++ pages/auth/firebase/index.js
@@ -1,46 +1,18 @@
-import { useEffect, useState } from "react";
-import Loading from "../../../components/utility/Loading";
 import { useRouter } from "next/router";
-import {
-  applyActionCode,
-  verifyPasswordResetCode,
-  confirmPasswordReset,
-} from "firebase/auth";
 import { auth } from "../../../firebase/config";
 import ResetPassword from "../../../components/auth/ResetPassword";
+import VerifyEmail from "../../../components/auth/VerifyEmail";
 
 const Firebase = () => {
   const router = useRouter();
   const { mode, oobCode } = router.query;
-
-  const [loading, setLoading] = useState(false);
 
   switch (mode) {
     case "resetPassword":
       return <ResetPassword mode={mode} actionCode={oobCode} />;
 
     case "verifyEmail":
-      applyActionCode(auth, actionCode);
-
-      setTimeout(() => {
-        router.push("/");
-      }, [5000]);
-
-      return (
-        <div className="min-h-screen bg-pink-100 flex items-center justify-center">
-          <div className="bg-white shadow-lg p-8 rounded-lg">
-            <h1 className="text-3xl text-purple-600 mb-8">
-              Congratulations! Your email has been successfully verified.
-            </h1>
-            <div className="flex justify-center gap-2">
-              <h1 className="text-2xl">Redirecting</h1>
-              <div className="pt-4">
-                <Loading />
-              </div>
-            </div>
-          </div>
-        </div>
-      );
+      return <VerifyEmail auth={auth} actionCode={oobCode} />;
 
     case "recoverEmail":
       break;
