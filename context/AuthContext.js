075e80e388a2cd4904e2bd39932ad18fb964c44d
immutable-bee <immutablebee@gmail.com>
2023-07-27 13:56:06 -0600

    add user membership to auth context

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,6 +1,7 @@
 import { onAuthStateChanged, getAuth } from "firebase/auth";
 import app from "@/firebase/config";
 import { createContext, useContext, useEffect, useState } from "react";
+import getDocument from "../firebase/firestore/getData";
 
 const auth = getAuth(app);
 
@@ -10,14 +11,24 @@ export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [membership, setMembership] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         setUser(user);
+        const { result, error } = await getDocument("users", user.uid);
+        if (error) {
+          console.error("Failed to fetch user document:", error);
+        } else if (result && result.exists()) {
+          setMembership(result.data().membership || null);
+        } else {
+          setMembership(null);
+        }
       } else {
         setUser(null);
+        setMembership(null);
       }
       setLoading(false);
     });
@@ -26,7 +37,7 @@ export const AuthContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, membership }}>
       {loading ? <div>Loading...</div> : children}
     </AuthContext.Provider>
   );
